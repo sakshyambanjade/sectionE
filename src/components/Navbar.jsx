6bea0a531fd93137dc5a6c3edb6760bd9fba108d
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2024-01-14 19:49:22 +0545

    navbar added bs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,61 @@
-import React, { Component } from 'react'
-import '../Style/Navbar.css'
-import DropNav from '../components/DropNav'
-export default class Navbar extends Component {
-  render() {
-    return (
-      <>
-      <div class="topnav" id="myTopnav">
-      <a href="#home" class="active">SECTION-E</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-    </div>
-      </>
-    )
-  }
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+export default function App() {
+  const [openBasic, setOpenBasic] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Section-E</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenBasic(!openBasic)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar open={openBasic}>
+          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='./'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                  Team
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem link href='./team1'>Team-1</MDBDropdownItem>
+                  <MDBDropdownItem link href='./team2'>Team-2</MDBDropdownItem>
+                  <MDBDropdownItem link href='./team3'>Team-3</MDBDropdownItem>
+                  <MDBDropdownItem link href='./team4'>Team-4</MDBDropdownItem>
+                  <MDBDropdownItem link href='./team5'>Team-5</MDBDropdownItem>
+                  <MDBDropdownItem link href='./team6'>Team-6</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
 }
